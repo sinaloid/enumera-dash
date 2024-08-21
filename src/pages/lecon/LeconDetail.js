50e409b243b255acd8e45caeabcfdcd74ee471d5
Sinali Dionou <sinaloid@gmail.com>
2024-08-21 17:32:49 +0000

    activation de tinymce, apose api

--- src/pages/lecon/LeconDetail.js
+++ src/pages/lecon/LeconDetail.js
@@ -10,6 +10,8 @@ import { useParams } from "react-router-dom";
 import TextEditor from "../../Components/TextEditor";
 import InputField from "../../Components/InputField";
 import DOMPurify from "dompurify";
+import { useRef } from "react";
+import { Editor } from "@tinymce/tinymce-react";
 
 const initData = {
   label: "",
@@ -17,6 +19,7 @@ const initData = {
   type: "pdf",
   lecon: "",
   description: "",
+  editorType: "blocknotejs",
 };
 const LeconDetail = () => {
   const authCtx = useContext(AppContext);
@@ -79,6 +82,18 @@ const LeconDetail = () => {
       } else {
         handleSubmit(values);
       }
+    },
+  });
+
+  const formikCoursFile = useFormik({
+    initialValues: initData,
+    //validationSchema: validateData,
+    onSubmit: (values) => {
+      values.label = lecon.label;
+      values.abreviation = lecon.abreviation;
+      values.lecon = lecon.slug;
+      values.description = cours;
+      sendCoursFile(values);
     },
   });
 
@@ -185,6 +200,29 @@ const LeconDetail = () => {
     });
   };
 
+  const sendCoursFile = (data) => {
+    //setShowModal(true)
+    toast.promise(request.post("convert-doc-to-html", data, header), {
+      pending: "Veuillez patienté...",
+      success: {
+        render({ data }) {
+          console.log(data);
+          const res = data;
+          setRefresh(refresh + 1);
+          return res.data.message;
+        },
+      },
+      error: {
+        render({ data }) {
+          console.log(data);
+          return data.response.data.errors
+            ? data.response.data.errors
+            : data.response.data.error;
+        },
+      },
+    });
+  };
+
   return (
     <>
       <div className="card p-4 border">
@@ -225,17 +263,45 @@ const LeconDetail = () => {
                 <ViewCours
                   data={lecon.cours}
                   formik={formik}
+                  formikCoursFile={formikCoursFile}
                   setCours={setCours}
                   file={file}
                 />
               </>
             ) : (
               <>
-                <TextEditor
-                  title={"Editeur de cours"}
-                  setValue={setCours}
-                  file={file}
-                />
+                <div className="d-flex">
+                  <div className="me-auto">
+                    <InputField
+                      type={"select"}
+                      name="editorType"
+                      formik={formik}
+                      placeholder="Sélectionnez un editeur"
+                      //label={"Sélectionnez une periode"}
+                      options={[
+                        { slug: "blocknotejs", label: "BlockNoteJs" },
+                        { slug: "tinymce", label: "Tinymce" },
+                      ]}
+                      //callback={editorType}
+                    />
+                  </div>
+                  <ImportCours formikCoursFile={formikCoursFile} />
+                </div>
+                {formik.values.editorType === "blocknotejs" && (
+                  <TextEditor
+                    title={"Editeur de cours"}
+                    setValue={setCours}
+                    file={file}
+                  />
+                )}
+                {formik.values.editorType === "tinymce" && (
+                  <Tinymce
+                    title={"Editeur de cours"}
+                    formik={formik}
+                    setValue={setCours}
+                    file={file}
+                  />
+                )}
                 <div className="d-flex justify-content-center mt-3">
                   <button
                     className="btn btn-primary w-75"
@@ -299,7 +365,9 @@ const LeconDetail = () => {
                     <div className="d-flex justify-content-between rounded-5">
                       <div>
                         <span>{data.original_name}</span> <br />
-                        <span className="fw-bold">{"Taille : " + data.taille}</span>
+                        <span className="fw-bold">
+                          {"Taille : " + data.taille}
+                        </span>
                       </div>
                       <br />
                       <div>
@@ -328,7 +396,7 @@ const LeconDetail = () => {
   );
 };
 
-const ViewCours = ({ data, formik, setCours, file }) => {
+const ViewCours = ({ data, formik, formikCoursFile, setCours, file }) => {
   const [view, setView] = useState("view");
   useEffect(() => {
     formik.setFieldValue("slug", data.slug);
@@ -362,9 +430,24 @@ const ViewCours = ({ data, formik, setCours, file }) => {
 
     console.log(videos);
   };
+
   return (
     <>
-      <div className="d-flex justify-content-end mb-3">
+      <div className="d-flex  mb-3">
+        <div className="me-auto">
+          <InputField
+            type={"select"}
+            name="editorType"
+            formik={formik}
+            placeholder="Sélectionnez un editeur"
+            //label={"Sélectionnez une periode"}
+            options={[
+              { slug: "blocknotejs", label: "BlockNoteJs" },
+              { slug: "tinymce", label: "Tinymce" },
+            ]}
+            //callback={editorType}
+          />
+        </div>
         <button
           className="btn btn-outline-primary me-1"
           onClick={(e) => changeView(e, "view")}
@@ -377,25 +460,38 @@ const ViewCours = ({ data, formik, setCours, file }) => {
         >
           Modifier
         </button>
-        <button className="btn btn-outline-danger">De publier</button>
+        <button className="btn btn-outline-danger me-1">De publier</button>
+        <ImportCours formikCoursFile={formikCoursFile} />
       </div>
       {view === "view" && (
         <>
           <div
+            className="row"
             dangerouslySetInnerHTML={{
-              __html: DOMPurify.sanitize(data?.description),
+              __html: data?.description,
             }}
           />
         </>
       )}
       {view === "edit" && (
         <>
-          <TextEditor
-            title={"Modification du cours"}
-            replaceData={data?.description}
-            setValue={setCours}
-            file={file}
-          />
+          {formik.values.editorType === "blocknotejs" && (
+            <TextEditor
+              title={"Modification du cours"}
+              replaceData={data?.description}
+              setValue={setCours}
+              file={file}
+            />
+          )}
+          {formik.values.editorType === "tinymce" && (
+            <Tinymce
+              title={"Modification du cours"}
+              replaceData={data?.description}
+              setValue={setCours}
+              file={file}
+            />
+          )}
+
           <div className="d-flex justify-content-center mt-3">
             <button
               className="btn btn-primary w-75"
@@ -406,6 +502,267 @@ const ViewCours = ({ data, formik, setCours, file }) => {
           </div>
         </>
       )}
+    </>
+  );
+};
+
+const Tinymce = ({ title, setValue, replaceData, file, editable=true }) => {
+  const editorRef = useRef(null);
+  const log = () => {
+    if (editorRef.current) {
+      //console.log(editorRef.current.getContent());
+      editorRef.current.setContent(replaceData)
+    }
+  };
+  useEffect(() => {
+    if (replaceData) {
+      //replaceBlocks();
+    }
+    if (editorRef.current) {
+      //console.log(editorRef.current.getContent());
+      editorRef.current = (replaceData)
+    }
+  }, [replaceData]);
+
+  return (
+    <>
+      <Editor
+        tinymceScriptSrc={`${process.env.PUBLIC_URL}/tinymce/tinymce.min.js`}
+        licenseKey="gpl"
+        onInit={(_evt, editor) => (editorRef.current = editor)}
+        initialValue={replaceData}
+        init={{
+          selector: "textarea#full-featured",
+          plugins:
+            "code preview casechange importcss tinydrive searchreplace autolink autosave save directionality advcode visualblocks visualchars fullscreen image link math media mediaembed codesample table charmap pagebreak nonbreaking anchor tableofcontents insertdatetime advlist lists checklist wordcount tinymcespellchecker a11ychecker editimage help formatpainter permanentpen pageembed charmap tinycomments mentions quickbars linkchecker emoticons advtable footnotes mergetags autocorrect typography advtemplate markdown revisionhistory",
+          tinydrive_token_provider: "URL_TO_YOUR_TOKEN_PROVIDER",
+          tinydrive_dropbox_app_key: "YOUR_DROPBOX_APP_KEY",
+          tinydrive_google_drive_key: "YOUR_GOOGLE_DRIVE_KEY",
+          tinydrive_google_drive_client_id: "YOUR_GOOGLE_DRIVE_CLIENT_ID",
+          paste_data_images: true,
+          mobile: {
+            plugins:
+              "code preview casechange importcss tinydrive searchreplace autolink autosave save directionality advcode visualblocks visualchars fullscreen image link math media mediaembed codesample table charmap pagebreak nonbreaking anchor tableofcontents insertdatetime advlist lists checklist wordcount tinymcespellchecker a11ychecker help formatpainter pageembed charmap mentions quickbars linkchecker emoticons advtable footnotes mergetags autocorrect typography advtemplate",
+          },
+          menu: {
+            tc: {
+              title: "Comments",
+              items: "addcomment showcomments deleteallconversations",
+            },
+          },
+          menubar: "file edit view insert format tools table tc help",
+          toolbar:
+            "code undo redo | revisionhistory | aidialog aishortcuts | blocks fontsizeinput | bold italic | align numlist bullist | link image | table math media pageembed | lineheight  outdent indent | strikethrough forecolor backcolor formatpainter removeformat | charmap emoticons checklist | code fullscreen preview | save print | pagebreak anchor codesample footnotes mergetags | addtemplate inserttemplate | addcomment showcomments | ltr rtl casechange | spellcheckdialog a11ycheck", // Note: if a toolbar item requires a plugin, the item will not present in the toolbar if the plugin is not also loaded.
+          autosave_ask_before_unload: true,
+          autosave_interval: "30s",
+          autosave_prefix: "{path}{query}-{id}-",
+          autosave_restore_when_empty: false,
+          autosave_retention: "2m",
+          image_advtab: true,
+          typography_rules: [
+            "common/punctuation/quote",
+            "en-US/dash/main",
+            "common/nbsp/afterParagraphMark",
+            "common/nbsp/afterSectionMark",
+            "common/nbsp/afterShortWord",
+            "common/nbsp/beforeShortLastNumber",
+            "common/nbsp/beforeShortLastWord",
+            "common/nbsp/dpi",
+            "common/punctuation/apostrophe",
+            "common/space/delBeforePunctuation",
+            "common/space/afterComma",
+            "common/space/afterColon",
+            "common/space/afterExclamationMark",
+            "common/space/afterQuestionMark",
+            "common/space/afterSemicolon",
+            "common/space/beforeBracket",
+            "common/space/bracket",
+            "common/space/delBeforeDot",
+            "common/space/squareBracket",
+            "common/number/mathSigns",
+            "common/number/times",
+            "common/number/fraction",
+            "common/symbols/arrow",
+            "common/symbols/cf",
+            "common/symbols/copy",
+            "common/punctuation/delDoublePunctuation",
+            "common/punctuation/hellip",
+          ],
+          typography_ignore: ["code"],
+          advtemplate_list: () => {
+            return Promise.resolve([
+              {
+                id: "1",
+                title: "Resolving tickets",
+                content:
+                  "<p>As we have not heard back from you in over a week, we have gone ahead and resolved your ticket.</p>",
+              },
+              {
+                id: "2",
+                title: "Quick replies",
+                items: [
+                  {
+                    id: "3",
+                    title: "Message received",
+                    content:
+                      "<p>Just a quick note to say we have received your message, and will get back to you within 48 hours.</p>",
+                  },
+                  {
+                    id: "4",
+                    title: "Progress update",
+                    content:
+                      "</p>Just a quick note to let you know we are still working on your case</p>",
+                  },
+                ],
+              },
+            ]);
+          },
+          link_list: [
+            { title: "My page 1", value: "https://www.tiny.cloud" },
+            { title: "My page 2", value: "http://www.moxiecode.com" },
+          ],
+          
+          importcss_append: true,
+          height: 600,
+          image_caption: true,
+          quickbars_selection_toolbar:
+            "bold italic | quicklink h2 h3 blockquote quickimage quicktable",
+          noneditable_class: "mceNonEditable",
+          toolbar_mode: "sliding",
+          spellchecker_ignore_list: [
+            "Ephox",
+            "Moxiecode",
+            "tinymce",
+            "TinyMCE",
+          ],
+          tinycomments_mode: "embedded",
+          content_style: ".mymention{ color: gray; }",
+          contextmenu: "link image editimage table configurepermanentpen",
+          a11y_advanced_options: true,
+          //skin: useDarkMode ? 'oxide-dark' : 'oxide',
+          //content_css: useDarkMode ? 'dark' : 'default',
+          /*
+          The following settings require more configuration than shown here.
+          For information on configuring the mentions plugin, see:
+          https://www.tiny.cloud/docs/tinymce/6/mentions/.
+          */
+          mentions_selector: ".mymention",
+          //  mentions_fetch: mentions_fetch, // TODO: Implement mentions_fetch
+          //mentions_menu_hover: mentions_menu_hover, // TODO: Implement mentions_menu_hover
+          //mentions_menu_complete: mentions_menu_complete, // TODO: Implement mentions_menu_complete
+          //mentions_select: mentions_select, // TODO: Implement mentions_select
+          images_upload_handler: function (blobInfo, success, failure) {
+            // Gérer l'upload des images ici si vous voulez les télécharger sur un serveur
+            //alert('ok')
+          },
+          automatic_uploads: true,
+          mentions_item_type: "profile",
+          autocorrect_capitalize: true,
+          mergetags_list: [
+            {
+              title: "Client",
+              menu: [
+                {
+                  value: "Client.LastCallDate",
+                  title: "Call date",
+                },
+                {
+                  value: "Client.Name",
+                  title: "Client name",
+                },
+              ],
+            },
+            {
+              title: "Proposal",
+              menu: [
+                {
+                  value: "Proposal.SubmissionDate",
+                  title: "Submission date",
+                },
+              ],
+            },
+            {
+              value: "Consultant",
+              title: "Consultant",
+            },
+            {
+              value: "Salutation",
+              title: "Salutation",
+            },
+          ],
+          revisionhistory_fetch: () => {
+            // Implement the fetch function for the revision history plugin
+            return Promise.resolve([
+              {
+                revisionId: "1",
+                createdAt: "2023-11-24T22:26:21.578Z",
+                content: "<p>Initial content</p>",
+              },
+            ]);
+          },
+        }}
+      />
+    </>
+  );
+};
+
+const ImportCours = ({ formikCoursFile }) => {
+  return (
+    <>
+      <div className="d-inline-block">
+        <button
+          className="btn btn-outline-danger"
+          data-bs-toggle="modal"
+          data-bs-target="#import"
+        >
+          Importer
+        </button>
+      </div>
+
+      <div className="modal fade" id="import">
+        <div className="modal-dialog modal-dialog-centered modal-lg">
+          <div className="modal-content">
+            <div className="modal-header border-0">
+              <h4 className="modal-title text-meduim text-bold">
+                Importation du cours à travers un document word
+              </h4>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+              ></button>
+            </div>
+
+            <div className="modal-body">
+              <form onSubmit={formikCoursFile.handleSubmit}>
+                <InputField
+                  type={"file"}
+                  name="file"
+                  formik={formikCoursFile}
+                  placeholder="Nom de la classe"
+                  label={"Sélectionnez le cours en document word"}
+                />
+                <div className="d-flex justify-content-start border-0">
+                  <button
+                    type="reset"
+                    className="btn btn-secondary me-2"
+                    data-bs-dismiss="modal"
+                  >
+                    Annuler
+                  </button>
+                  <button
+                    type="submit"
+                    className="btn btn-primary"
+                    data-bs-dismiss="modal"
+                  >
+                    Enregistrer
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
