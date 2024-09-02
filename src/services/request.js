9635cd3b788adf772151332477b8c0836cea086d
Sinali Dionou <sinaloid@gmail.com>
2024-09-02 11:16:00 +0000

    changement d'url

--- src/services/request.js
+++ src/services/request.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 //export const URL = "http://127.0.0.1:8000"
 //export const URL = "http://192.168.11.150:8000"
+//export const URL = "https://wilofo-api.enumera.tech"
 
-export const BASE_URL = "https://wilofo-api.enumera.tech";
+export const URL = "https://wilofo-api.enumera.tech";
 //export const URL = "https://api.enumera.tech"
 
 const request = axios.create({
